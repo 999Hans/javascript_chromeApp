3590b5c55f61aba1d03acbb888003849379d7251
HanyeongOh <61238500+HanyeongOh@users.noreply.github.com>
2020-12-07 22:58:56 +0900

    First commit, make clock page by javascript

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+
+  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function init() {
+  setInterval(getTime, 1000);
+}
+
+init();
