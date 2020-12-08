8150965bf2114aa2ce42e83e33413af31dcc948b
HanyeongOh <61238500+HanyeongOh@users.noreply.github.com>
2020-12-08 23:09:43 +0900

    make seconds clock by setInterval and if num < 10, plus 0 forehead to the num with if sentence and ` if sentence

--- clock.js
+++ clock.js
@@ -1,16 +1,26 @@
 const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
+function plusZero(num) {
+  // if (num < 10) {
+  //   return `0${num}`;
+  // } else {
+  //   return `${num}`;
+  // }
+  return `${num < 10 ? `0${num}` : `${num}`}`;
+}
+
 function getTime() {
   const date = new Date();
-  const seconds = date.getSeconds();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
+  const seconds = plusZero(date.getSeconds());
+  const minutes = plusZero(date.getMinutes());
+  const hours = plusZero(date.getHours());
 
   clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function init() {
+  getTime();
   setInterval(getTime, 1000);
 }
 
