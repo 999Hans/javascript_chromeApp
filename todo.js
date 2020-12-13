1e7dd7cc7f39c5ecfb76f02d586ab064ea7841e9
HanyeongOh <61238500+HanyeongOh@users.noreply.github.com>
2020-12-13 18:07:32 +0900

    making to do list...

--- todo.js
+++ todo.js
@@ -1,9 +1,29 @@
 const todoForm = document.querySelector(".js-todoForm"),
-  todoInput = todoForm.querySelector("input");
+  todoInput = todoForm.querySelector("input"),
+  todoList = document.querySelector(".js-todoList"),
+  checkList = document.querySelector(".js-checkList");
+
+function makeList(text) {
+  //   const li = document.createElement("li");
+  //   li.innerText = text;
+  //   todoList.appendChild(li);
+  const checkContainer = document.createElement("div");
+  const check = document.createElement("input");
+  const checkContent = document.createElement("span");
+  const br = document.createElement("br");
+  check.type = "checkbox";
+  checkContent.innerText = text;
+  checkContainer.appendChild(check);
+  checkContainer.appendChild(checkContent);
+  checkContainer.appendChild(br);
+
+  checkList.appendChild(checkContainer);
+}
 
 function todoSubmit(event) {
   event.preventDefault();
-  console.log(todoInput.value);
+  localStorage.setItem("todo", JSON.stringify(todoInput.value));
+  makeList(JSON.parse(localStorage.getItem("todo")));
 }
 
 function init() {
